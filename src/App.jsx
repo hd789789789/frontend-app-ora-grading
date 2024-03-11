0e144be080cd1f4604856d13999916308d006949
hd789789789 <65296761+hd789789789@users.noreply.github.com>
2024-03-11 10:00:53 +0700

    Add Vietnamese Language

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,58 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import Footer from '@edx/frontend-component-footer';
-import { LearningHeader as Header } from '@edx/frontend-component-header';
+// import Footer from '@edx/frontend-component-footer';
+import { LearningHeader as Header } from "@edx/frontend-component-header";
 
-import { selectors } from 'data/redux';
+import { selectors } from "data/redux";
 
-import DemoWarning from 'containers/DemoWarning';
-import CTA from 'containers/CTA';
-import ListView from 'containers/ListView';
+import DemoWarning from "containers/DemoWarning";
+import CTA from "containers/CTA";
+import ListView from "containers/ListView";
 
-import './App.scss';
-import Head from './components/Head';
+import "./App.scss";
+import Head from "./components/Head";
 
 export const App = ({ courseMetadata, isEnabled }) => (
-  <Router>
-    <div>
-      <Head />
-      <Header
-        courseTitle={courseMetadata.title}
-        courseNumber={courseMetadata.number}
-        courseOrg={courseMetadata.org}
-        data-testid="header"
-      />
-      {!isEnabled && <DemoWarning />}
-      <CTA />
-      <main data-testid="main">
-        <ListView />
-      </main>
-      <Footer logo={process.env.LOGO_POWERED_BY_OPEN_EDX_URL_SVG} data-testid="footer" />
-    </div>
-  </Router>
+    <Router>
+        <div>
+            <Head />
+            <Header
+                courseTitle={courseMetadata.title}
+                courseNumber={courseMetadata.number}
+                courseOrg={courseMetadata.org}
+                data-testid="header"
+            />
+            {!isEnabled && <DemoWarning />}
+            <CTA />
+            <main data-testid="main">
+                <ListView />
+            </main>
+            {/* <Footer logo={process.env.LOGO_POWERED_BY_OPEN_EDX_URL_SVG} data-testid="footer" /> */}
+        </div>
+    </Router>
 );
 App.defaultProps = {
-  courseMetadata: {
-    title: '',
-    number: null,
-    org: '',
-  },
+    courseMetadata: {
+        title: "",
+        number: null,
+        org: "",
+    },
 };
 App.propTypes = {
-  courseMetadata: PropTypes.shape({
-    title: PropTypes.string,
-    number: PropTypes.string,
-    org: PropTypes.string,
-  }),
-  isEnabled: PropTypes.bool.isRequired,
+    courseMetadata: PropTypes.shape({
+        title: PropTypes.string,
+        number: PropTypes.string,
+        org: PropTypes.string,
+    }),
+    isEnabled: PropTypes.bool.isRequired,
 };
 
 export const mapStateToProps = (state) => ({
-  courseMetadata: selectors.app.courseMetadata(state),
-  isEnabled: selectors.app.isEnabled(state),
+    courseMetadata: selectors.app.courseMetadata(state),
+    isEnabled: selectors.app.isEnabled(state),
 });
 
 export default connect(mapStateToProps)(App);
